898a3f4b41059ebac0dfd120a2e748c30f132266
jonathancode <parkj97@gmail.com>
2021-06-29 20:15:45 -0700

    model tweak migrate model

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.db.models import Model
 from django.contrib.auth.models import User
-from django.db.models.fields.related import ManyToManyField 
+from django.core.exceptions import ValidationError 
 
 
+
+def validate_post_title(post):
+    if not post.title.length:
+        raise ValidationError("Please enter a title")
+    else:
+        return post 
 
 
 #One user has one profile. One profile has one user. 
 class Profile(Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(min_length = 1, max_length= 30)
+    name = models.CharField(max_length = 30)
     profile_location = models.CharField(max_length = 80, blank=True, null=True)
     join_date = models.DateTimeField(auto_now_add=True)
     image = models.FileField(upload_to="images", default="settings.MEDIA_ROOT/images/nolocation.png")
@@ -21,7 +27,7 @@ class Profile(Model):
 
 class Post(Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="posts")
-    title = models.CharField(min_length = 3, max_length = 30)
+    title = models.CharField(validators = [validate_post_title], max_length = 30)
     content = models.CharField(max_length = 300)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -32,7 +38,7 @@ class Post(Model):
 class Location(Model):
     name = models.CharField(max_length = 100)
     image = models.FileField(upload_to="images", default="settings.MEDIA_ROOT/images/noprofile.png")
-    posts = models.ForeignKey(Post, null=True, blank=True, related_name="post_location") 
+    posts = models.ForeignKey(Post, on_delete = models.CASCADE, null=True, blank=True, related_name="post_location") 
 
     def __str__(self):
         return self.name 
