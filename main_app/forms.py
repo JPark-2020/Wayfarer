f1b593846a2cb208f6139a7e9a56956b036f7a34
jonathancode <parkj97@gmail.com>
2021-07-01 00:03:19 -0700

    signup working

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,5 @@
 from django.forms import ModelForm 
 from .models import Profile, Post  
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User 
-from django import forms 
 
 class ProfileUpdate(ModelForm):
     class Meta:
@@ -15,10 +12,3 @@ class PostCreate(ModelForm):
         fields = ['title', 'content']
 
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.TextInput()
-    last_name= forms.TextInput()
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,8 @@ from django.views import View
 from .models import Profile, Location, Post  
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm 
-from .forms import ProfileUpdate, PostCreate, UserRegisterForm 
+from django.contrib.auth import login
+from .forms import ProfileUpdate, PostCreate
 from django.http import HttpResponseRedirect
 
 
@@ -31,31 +32,22 @@ class ProfilePage(View):
             if form.is_valid():
                 form.save() 
         return render(request, "profile.html", context)
-
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('profile_page')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'registration/signup.html', {"form":form})
     
-# class SignUp(View):
-#     def get(self, request):
-#         form = UserCreationForm()
-#         context = {"form": form}
-#         return render(request, "registration/signup.html", context)
-
-#     def post(self, request):
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             Profile.objects.create(user=user)
-#             return redirect('/profile')
-#         else:
-#             return redirect("/")
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(user=user)
+            login(request, user)
+            return redirect("profile_page")
+        else:
+            return redirect("signup_page")
 
 class LocationList(View):
     def get(self, request):
@@ -84,31 +76,6 @@ class LocationDetail(View):
             form.save()
 
         return HttpResponseRedirect(self.request.path_info)
-    
-    
-    
-    
-    # def post(self, request, locationname):
-    #     current_user = request.user 
-    #     user_profile = Profile.objects.get(user_id = current_user)
-    #     form = PostCreate() 
-    #     found_location = Location.objects.get(name = locationname)
-    #     context = {"found_location": found_location, "form": form}
-    #     if request.method == "POST":
-    #         post_author = user_profile.user_id
-    #         post_title = request.POST['title']
-    #         post_content = request.POST['content']
-    #         print(post_author)
-    #         print(post_title)
-    #         print(post_content)
-    #         if form.is_valid():
-    #             Post.objects.create(
-    #                 title = post_title, 
-    #                 content = post_content,
-    #                 author_id = post_author,
-    #             )
-    #             form.save()
-    #         return render(request,'location-detail.html', context)
                 
 
 class PostList(View):
